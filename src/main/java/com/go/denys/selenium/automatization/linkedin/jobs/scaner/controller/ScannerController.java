5dd8d830fc8248c56d106b99c5ca3470c4d0d01c
DenysHo <denys.honcharenko.8@gmail.com>
2024-10-19 10:52:45 +0200

    add simple web UI

--- src/main/java/com/go/denys/selenium/automatization/linkedin/jobs/scaner/controller/ScannerController.java
+++ src/main/java/com/go/denys/selenium/automatization/linkedin/jobs/scaner/controller/ScannerController.java
@@ -1,16 +1,20 @@
 package com.go.denys.selenium.automatization.linkedin.jobs.scaner.controller;
 
-import com.go.denys.selenium.automatization.linkedin.jobs.scaner.dto.JobAd;
+import com.go.denys.selenium.automatization.linkedin.jobs.scaner.dto.ScannerFilter;
 import com.go.denys.selenium.automatization.linkedin.jobs.scaner.service.ScannerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import static com.go.denys.selenium.automatization.linkedin.jobs.scaner.enums.TimeRangeSelector.DAY;
 
-@RestController
+@Controller
 public class ScannerController {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(ScannerService.class);
     private final ScannerService scannerService;
 
     @Autowired
@@ -18,10 +22,40 @@ public class ScannerController {
         this.scannerService = scannerService;
     }
 
-    @GetMapping("/scan")
-    public String start() {
-        List<JobAd> scan = scannerService.scan();
-        System.out.println("norm");
-        return "Ok";
+    @GetMapping("/")
+    @ResponseBody
+    public String initialize(@RequestParam(value = "message", required = false) String message) {
+        return "<html>" +
+                "<body>" +
+                "<h1>Welcome to my LinkedIn Vacancy Scraping Tools!</h1>" +
+                (message != null ? "<p>" + message + "</p>" : "") +
+                "<form action='/scan' method='post'>" +
+                "<button type='submit'>Click me to check new ad!</button>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
+
+    @PostMapping("/scan")
+    public String scan() {
+        ScannerFilter scannerFilter = ScannerFilter.builder()
+                .timeRange(DAY)
+                .keywords("Java Developer")
+                .location("Germany")
+                .limit(0)
+                .previous(true)
+                .build();
+
+        String message;
+        try {
+            scannerService.scan(scannerFilter);
+            message = "Action processed successfully!";
+        } catch (Exception e) {
+            logger.info("An error occurred: {}", e.getMessage(), e);
+            message = "Oops, something went wrong:(";
+        }
+
+        return "redirect:/?message=" + message;
+    }
+
 }
